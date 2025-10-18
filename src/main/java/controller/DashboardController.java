c5601a0918abee0ac603c4041700801e4054851b
UdaraDewshan <dewudara@gmail.com>
2025-10-18 16:49:43 +0530

    Add Dashboard Controller

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,7 +2,10 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class DashboardController {
 
@@ -11,7 +14,8 @@ public class DashboardController {
 
     @FXML
     void btnLoginAction(ActionEvent event) {
-
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/"))));
     }
 
 }
