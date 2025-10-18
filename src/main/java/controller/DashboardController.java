94a6a81d541896a797b1317238471d1f0c524cd7
UdaraDewshan <dewudara@gmail.com>
2025-10-18 16:41:40 +0530

    Add Dashboard Fxml

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DashboardController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    void btnLoginAction(ActionEvent event) {
+        
+    }
+
+}
