e64307485ec6b9e3f12a2c97cbce0ce590810fa8
UdaraDewshan <dewudara@gmail.com>
2025-10-20 20:26:14 +0530

    Add Item Fxml

---
+++ src/main/java/controller/ItemController.java
@@ -0,0 +1,85 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ItemController {
+
+    @FXML
+    private TableColumn<?, ?> Colcode;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colCatogory;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableView<?> tblItem;
+
+    @FXML
+    private TextField txtCategory;
+
+    @FXML
+    private TextField txtCatogary;
+
+    @FXML
+    private TextField txtDiscription;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    void btnAddAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackActiioon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateAction(ActionEvent event) {
+
+    }
+
+}
