826648b691b159a7a835da0859ce111b96ef37c7
UdaraDewshan <dewudara@gmail.com>
2025-10-21 21:28:08 +0530

    Add Employee Fxml

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,115 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class EmployeeController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colAdress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colEmpId;
+
+    @FXML
+    private TableColumn<?, ?> colJoinDate;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colNic;
+
+    @FXML
+    private TableColumn<?, ?> colPosition;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableColumn<?, ?> colStates;
+
+    @FXML
+    private TableView<?> tblEmployee;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtContactNo;
+
+    @FXML
+    private TextField txtDob;
+
+    @FXML
+    private TextField txtEmpId;
+
+    @FXML
+    private TextField txtJoinDate;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtNic;
+
+    @FXML
+    private TextField txtPosition;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    private TextField txtStatus;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) {
+
+    }
+
+}
